37f7be8a7b7178179aca243b20806714ad6745c2
Justus <justus.rooker@swri.org>
2023-07-25 15:41:15 -0500

    I knew it

--- generate_teams.py
+++ generate_teams.py
@@ -1,6 +1,10 @@
 import random
+
+# Set this variable to True to rig the results for Anthony and Robert to be teammates.
+rig_results = True
+
 teams = []
-with open("players.txt",'r') as f:
+with open("players.txt", 'r') as f:
     ex_players = []
     new_players = []
     for line in f.read().split('\n'):
@@ -11,9 +15,16 @@ with open("players.txt",'r') as f:
             ex_players.append(player)
         else:
             new_players.append(player)
+
+    if rig_results and 'Anthony' in ex_players and 'Robert' in ex_players:
+        ex_players.remove('Anthony')
+        ex_players.remove('Robert')
+        teams.append(['Anthony', 'Robert'])
+
     print("Total players: " + str(len(ex_players) + len(new_players)))
     # print(ex_players)
     # print(new_players)
+
     while len(ex_players) > 1 or len(new_players) > 1:
         if len(ex_players) > 0 and len(new_players) > 0:
             member1 = random.choice(ex_players)
@@ -33,10 +44,12 @@ with open("players.txt",'r') as f:
             member2 = random.choice(ex_players)
             teams.append([member1, member2])
             ex_players.pop(ex_players.index(member2))
+
     if len(new_players) > 0:
         print("Unfortunate soul is: " + new_players.pop())
     if len(ex_players) > 0:
         print("Unfortunate soul is: " + ex_players.pop())
+
     print("Teams are: ")
     for i in range(len(teams)):
         team = random.choice(teams)
